a515c2135997b7aacf65c79afffbbf4de929f460
gamze-yil <fztgamzeyilmaz@gmail.com>
2025-12-11 10:38:47 +0100

    initial spring boot project

---
+++ src/main/java/com/gamze/health_pain_tracker/HealthPainTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.gamze.health_pain_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPainTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPainTrackerApplication.class, args);
+	}
+
+}
